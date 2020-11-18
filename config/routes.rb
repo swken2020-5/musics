12d068a73a6eb5abf86088e244bbd06617d57f74
Daiki Moriuchi <1811140022g@ed.fuk.kindai.ac.jp>
2020-11-18 06:47:52 +0000

    model_controllerを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'top/login'
+  get 'tweets/index'
+  get 'tweets/new'
+  get 'users/index'
+  get 'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
