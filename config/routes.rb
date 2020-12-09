c1477bdd69b13e780fee2ae6657cc904dbf82907
Asahi Inoue <1811140066c@ed.fuk.kindai.ac.jp>
2020-12-09 04:57:46 +0000

    routesの取得

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-
   resources :tweets
   root 'users#index'
   get 'users/new'
   post 'top/login'
   get 'top/login_form'
+  post 'top/login'
   get 'top/logout'
-  post 'tweets/index'
-  get 'tweets/new'
-  
   resources :tweets
   resources :users
   resources :likes
